d047945d6ea41353d2df46919876e7f55f90854e
JonatanSoruco <sorucojonatan@gmail.com>
2024-10-15 17:24:24 -0300

    agregando tpi

---
+++ TPI-Backend/src/main/java/frc/utn/TPI_Backend/TpiBackendApplication.java
@@ -0,0 +1,13 @@
+package frc.utn.TPI_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpiBackendApplication.class, args);
+	}
+
+}
---
+++ TPI-Backend/src/test/java/frc/utn/TPI_Backend/TpiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package frc.utn.TPI_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
