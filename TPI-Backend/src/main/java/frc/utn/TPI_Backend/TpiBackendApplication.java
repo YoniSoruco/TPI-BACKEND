0edf1b2a5fd086e7cae47b8db562a3156faff841
JonatanSoruco <sorucojonatan@gmail.com>
2024-10-15 17:34:46 -0300

    agregando bdd

--- TPI-Backend/src/main/java/frc/utn/TPI_Backend/TpiBackendApplication.java
+++ TPI-Backend/src/main/java/frc/utn/TPI_Backend/TpiBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TpiBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TpiBackendApplication.class, args);
+
 	}
 
 }
