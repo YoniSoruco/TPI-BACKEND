62d789e514d4f5ef406957bd34000be91d6a2f7c
madsword47 <lucas.original.47@gmail.com>
2024-11-12 16:43:45 -0300

    completé MarcaDTO, PruebaDTO y ModeloDTO

--- Pruebas/src/main/java/frc/utn/TPI_Backend/Pruebas/dto/MarcaDTO.java
+++ Pruebas/src/main/java/frc/utn/TPI_Backend/Pruebas/dto/MarcaDTO.java
@@ -1,4 +1,15 @@
 package frc.utn.TPI_Backend.Pruebas.dto;
 
+import lombok.Data;
+
+@Data
 public class MarcaDTO {
+    private int id;
+    private String nombre;
+
+    public MarcaDTO(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
 }
+
--- Vehiculos/src/main/java/frc/utn/TPI_Backend/Vehiculos/dto/PruebaDTO.java
+++ Vehiculos/src/main/java/frc/utn/TPI_Backend/Vehiculos/dto/PruebaDTO.java
@@ -1,7 +1,26 @@
 package frc.utn.TPI_Backend.Vehiculos.dto;
 
+import lombok.Data;
+
+import java.text.DateFormat;
+
+@Data
 public class PruebaDTO {
-
-
-
+    private int id;
+    private int idVehiculo;
+    private int idInteresado;
+    private int idEmpleado;
+    private DateFormat fechaHoraInicio;
+    private DateFormat fechaHoraFin;
+    private String comentarios;
+    public PruebaDTO(int id, int idVehiculo, int idInteresado, int idEmpleado, DateFormat fechaHoraInicio, DateFormat fechaHoraFin, String comentarios) {
+        this.id = id;
+        this.idVehiculo = idVehiculo;
+        this.idInteresado = idInteresado;
+        this.idEmpleado = idEmpleado;
+        this.fechaHoraInicio = fechaHoraInicio;
+        this.fechaHoraFin = fechaHoraFin;
+        this.comentarios = comentarios;
+    }
 }
+
